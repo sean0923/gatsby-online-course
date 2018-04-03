8cc346d002fe68580f67c841d1876ec8575fb328
Sean Hong <iamseanhong@gmail.com>
2018-04-02 21:28:07 -0700

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolorem consequuntur placeat tempora temporibus, impedit rem autem nulla aspernatur recusandae laudantium, maxime corporis distinctio. Voluptas quae eos accusamus dolor assumenda!</p>
+      </div>
+    )
+  }
+}
