cdd63b786bf49ad6a29b0e94f3c3af9ff3fbf1ed
Sean Hong <iamseanhong@gmail.com>
2018-04-03 08:08:03 -0700

    Add styled component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -26,12 +27,16 @@ const Header = () => (
           Gatsby
         </Link>
       </h1>
-    </div>
+    </HeaderContainer>
     <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/about'>About</Link></li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
     </ul>
-  </div>
-)
+  </HeaderWrapper>
+);
 
-export default Header
+export default Header;
