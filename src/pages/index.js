6e11a783c13095ba303ed1eab50dfe596b9940a6
Sean Hong <iamseanhong@gmail.com>
2018-04-03 22:32:14 -0700

    Add img to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Img from 'gatsby-image';
+
 
 const IndexPage = ({ data }) => (
   <div>
@@ -8,8 +8,6 @@ const IndexPage = ({ data }) => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-    <Img sizes={data.caveLightImg.sizes} />
-    <Img sizes={data.seaForestImg.sizes} />
     <p>{data.site.siteMetadata.title}</p>
     <p>{data.site.siteMetadata.test}</p>
     <p>{data.site.siteMetadata.test2}</p>
@@ -19,22 +17,12 @@ const IndexPage = ({ data }) => (
 export default IndexPage;
 
 export const query = graphql`
-  query SiteMeta {
+  query IndexPageQuery {
     site {
       siteMetadata {
         title
         test
         test2
-      }
-    }
-    caveLightImg: imageSharp(id: { regex: "/cave-light.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    seaForestImg: imageSharp(id: { regex: "/sea-forest.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
       }
     }
   }
