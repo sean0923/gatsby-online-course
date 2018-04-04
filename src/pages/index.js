539bff2846e1f935d181d448244027eebca82677
Sean Hong <iamseanhong@gmail.com>
2018-04-03 21:24:52 -0700

    Able to get progressive load img using many plugin and graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Img from 'gatsby-image';
 
 const IndexPage = ({ data }) => (
   <div>
@@ -7,6 +8,8 @@ const IndexPage = ({ data }) => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Img sizes={data.caveLightImg.sizes} />
+    <Img sizes={data.seaForestImg.sizes} />
     <p>{data.site.siteMetadata.title}</p>
     <p>{data.site.siteMetadata.test}</p>
     <p>{data.site.siteMetadata.test2}</p>
@@ -22,6 +25,16 @@ export const query = graphql`
         title
         test
         test2
+      }
+    }
+    caveLightImg: imageSharp(id: { regex: "/cave-light.jpg/" }) {
+      sizes(maxWidth: 1240) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    seaForestImg: imageSharp(id: { regex: "/sea-forest.jpg/" }) {
+      sizes(maxWidth: 1240) {
+        ...GatsbyImageSharpSizes
       }
     }
   }
