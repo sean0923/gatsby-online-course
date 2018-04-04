ffa9531453dff89ce25a4d9ef00c79f03e21a183
Sean Hong <iamseanhong@gmail.com>
2018-04-03 22:32:14 -0700

    Add img to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
 
 const HeaderWrapper = styled.div`
   background: #524763;
   margin-bottom: 1.45rem;
+  position: relative;
+  overflow: hidden;
 `;
 
 const HeaderContainer = styled.div`
   margin: 0 auto;
   max-width: 960px;
   padding: 1.45rem 1.0875rem;
+  position: relative;
+  height: 45vh;
+  /* z index can be applied only when position prop */
+  z-index: 2; 
 `;
 
-const Header = () => (
+const Header = ({ data }) => (
   <HeaderWrapper>
     <HeaderContainer>
       <h1 style={{ margin: 0 }}>
@@ -35,7 +42,18 @@ const Header = () => (
           <Link to="/about">About</Link>
         </li>
       </ul>
+      
     </HeaderContainer>
+    <Img
+      sizes={data.caveLightImg.sizes}
+      style={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '125%',
+      }}
+    />
   </HeaderWrapper>
 );
 
